811a908c27ca0df4e769c91713436a51e9072060
theprithwisingh <prithwisingh77@gmail.com>
2024-03-12 23:45:39 +0530

    fix

--- src/Config/Gemini.js
+++ src/Config/Gemini.js
@@ -6,7 +6,7 @@ import {
   } from "@google/generative-ai";
   
   const MODEL_NAME = "gemini-1.0-pro";
-  const API_KEY = "AIzaSyCuVq3w5wnEmpMJdyEOA-5qRpz3p4VuBr8";
+  const API_KEY = "";
   
   async function runChat(prompt) {
     const genAI = new GoogleGenerativeAI(API_KEY);
