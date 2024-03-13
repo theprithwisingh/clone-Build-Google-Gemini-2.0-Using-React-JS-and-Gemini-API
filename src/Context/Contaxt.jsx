e4c9b0924e5471f9e52230b3060117844d8e1135
theprithwisingh <prithwisingh77@gmail.com>
2024-03-13 15:37:08 +0530

    03:36 13/3

--- src/Context/Contaxt.jsx
+++ src/Context/Contaxt.jsx
@@ -11,6 +11,12 @@ const ContextProvider = (props)=>{
        const[loading,setLoading]=useState(false);
        const[resultData,setResultData]=useState("");
 
+       const delayPara=(index, nextWord)=>{
+           setTimeout(function (){
+            setResultData(prev=>prev+nextWord)
+           },30*index)
+       }
+
     const  onSent = async (prompt)=>{
 
         setResultData("");
@@ -18,7 +24,27 @@ const ContextProvider = (props)=>{
         setShowResult(true);
         setRecentPrompt(input);
        const response = await runChat(input);
-       setResultData(response);
+
+       let responseArray = response.split("**");
+       let newResponse="";
+       for(let i = 0; i < responseArray.length; i++)
+       {
+         if(i === 0 || i % 2 !== 1){
+            newResponse += responseArray[i];
+         }
+         else{
+           newResponse += "<b>" + responseArray[i] + "</b>";
+         }
+       }
+
+       let newResponse2=newResponse.split("*").join("</br>")
+       let newResponceArray = newResponse2.split("");
+       for(let i=0; i<newResponceArray.length; i++)
+       { 
+        const nextWord = newResponceArray[i];
+        delayPara(i,nextWord+"")
+
+       }
        setLoading(false);
        setInput("");
     }
